19c943133b2dcf275136aeb60fd5a7f24da81cb4
Minh Bui <tranduyminh.bui@gmail.com>
2020-03-02 18:17:13 +1100

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_one_attached :photo
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_one_attached :photo
   has_many :favorites, dependent: :destroy
 
   geocoded_by :address
