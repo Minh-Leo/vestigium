f540657ae6212e7f175a7ac50a6916d0ccc79bfa
Nikolai Pammer <npammer@gmail.com>
2020-03-04 11:57:46 +1100

    html css and some model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,19 +1,9 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[show]
-
-  def new
-    @article = Article.new
+  def show
+    @article = Article.find(params[:id]
   end
-
-  def show; end
 
   def index
     @articles = Article.all
-  end
-
-  private
-
-  def set_article
-    @article = Article.find(params[:id])
   end
 end
