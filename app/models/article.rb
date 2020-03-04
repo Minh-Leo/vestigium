66413650d9f5e52a3ab57ddb0db2ee59c332c822
Nikolai Pammer <npammer@gmail.com>
2020-03-04 14:02:23 +1100

    fixed models

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,5 +4,6 @@ class Article < ApplicationRecord
 
   validates :title, :description, :url, :publish_time, presence: true
   belongs_to :source
+  has_many :article_keywords
   has_many :keywords, through: :article_keywords
 end
--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -3,5 +3,6 @@ class Keyword < ApplicationRecord
   multisearchable against: [:word]
 
   validates :word, presence: true
+  has_many :article_keywords
   has_many :articles, through: :article_keywords
 end
