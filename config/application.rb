01d95787ac8196cbb6f7e1da1ea7a856a907719c
minh bui <tranduyminh.bui@gmail.com>
2020-03-06 15:46:15 +1100

    troubleshooting for heroku assets error

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Vestigium
         end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
