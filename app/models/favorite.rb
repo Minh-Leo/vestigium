ffa9cd1b80498be70bd7b36c09de04c8c9156cf9
minh bui <tranduyminh.bui@gmail.com>
2020-03-11 16:47:57 +1100

    favorite bug

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,5 +1,5 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :article
-  has_many :lists, through: :favorite_lists, dependent: :destroy
+  has_many :lists, through: :favorite_lists
 end
