b27fa889386a28f00e629423474bb00a8797427e
Minh Bui <tranduyminh.bui@gmail.com>
2020-03-11 11:48:51 +1100

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,16 +5,10 @@ import "../components/3Drenderers";
 
 import { analyze } from '../components/sentiment';
 
+import { initSortable } from '../plugins/init_sortable';
 import { initMapbox } from '../plugins/init_mapbox';
-
 import { initNotes } from '../plugins/init_notes';
 
 initMapbox();
 initNotes();
-
-import { initSortable } from '../plugins/init_sortable';
-
 initSortable();
-
-
-console.log(analyze("For 13 years, Puerto Rico has been in a man-made recession aggravated by natural disasters. In 2017 the island territory asked a federal court for bankruptcy protection, but its still grappling with $70 billion in debt. Puerto Ricos fiscal fiasco offers a By trying to tax its way out of fiscal disaster, the state makes matters worse. There’s a simple solution "));
