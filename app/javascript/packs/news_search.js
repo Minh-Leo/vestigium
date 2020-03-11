7cdf1325cb85841cc0e8f9c44a670cbc07ce19bf
minh bui <tranduyminh.bui@gmail.com>
2020-03-11 20:26:40 +1100

    styling and refining layout

--- app/javascript/packs/news_search.js
+++ app/javascript/packs/news_search.js
@@ -4,7 +4,6 @@ import { create3Dglobe } from "../components/3Drenderers";
 console.log('begin');
 
 // const createButton = document.getElementById('create');
-const searchButton = document.getElementById('submit-search-btn');
 const searchForm = document.getElementById('search-field-div');
 const searchField = document.getElementById('search-field');
 const threeContainer = document.getElementById('three-container');
