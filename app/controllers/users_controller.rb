a6d9afc67ffe8d572af28fa5e9142e146e4aa413
Nikolai Pammer <npammer@gmail.com>
2020-03-12 14:36:00 +1100

    different changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def update
     @user.update!(user_params)
     if @user.save
-      redirect_to user_path(@user)
+      redirect_to dashboard_path
     else
       render :new
     end
