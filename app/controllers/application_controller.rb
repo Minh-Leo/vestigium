ced3ffce0568c75390f0816f743c1b37ef9460ea
minh bui <tranduyminh.bui@gmail.com>
2020-03-31 15:41:49 +1100

    fixed heroku errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   add_flash_types :info, :error, :warning
 
   before_action :authenticate_user!
+  skip_before_action :verify_authenticity_token
+
   def after_sign_in_path_for(user)
     news_main_path
   end
