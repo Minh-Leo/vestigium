ad688f9a5d2e2e9647b9958565987acd8576dd50
minh bui <tranduyminh.bui@gmail.com>
2020-06-15 13:52:24 +1000

    added origin to cors gem

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Vestigium
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins ['http://localhost:3000','https://newsapi.org/']
+        origins ['https://www.vestigium.world','http://localhost:3000','https://newsapi.org/']
         resource '*', headers: :any, methods: :any
       end
     end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 module Vestigium
   Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins ['http://localhost:3000','https://newsapi.org/']
+      origins ['https://www.vestigium.world','http://localhost:3000','https://newsapi.org/']
       resource '*', headers: :any, methods: :any
     end
   end
